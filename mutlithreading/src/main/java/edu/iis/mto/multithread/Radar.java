ff0474d50908d0ca1fbea735b0f1d8388a7878c2
Jakub Mroczek <jakub.mroczek@tomtom.com>
2019-06-05 23:04:40 +0200

    Noticed that class Radar should not have been modified. Revert "moved launchingPatriotrs to Executor"
    This reverts commit 8f238b9835709a8c074581ad2ab919c7801a4db9.

--- mutlithreading/src/main/java/edu/iis/mto/multithread/Radar.java
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/Radar.java
@@ -1,15 +1,10 @@
 package edu.iis.mto.multithread;
 
-import java.util.concurrent.Executor;
-import java.util.stream.IntStream;
-
 public class Radar {
 	private PatriotBattery battery;
-	private Executor executor;
 
-	public Radar(PatriotBattery battery, Executor executor)  {
+	public Radar(PatriotBattery battery) {
 		this.battery = battery;
-		this.executor = executor;
 	}
 
 	public void notice(Scud enemyMissle) {
@@ -17,8 +12,15 @@ public class Radar {
 	}
 
 	private void launchPatriot() {
-		executor.execute(() -> {
-			IntStream.range(0, 10).forEach(x -> battery.launchPatriot());
-		});
+		Runnable launchPatriotTask = new Runnable() {
+			public void run() {
+				for (int i = 0; i < 10; i++) {
+					battery.launchPatriot();
+				}
+			}
+		};
+
+		Thread launchingThread = new Thread(launchPatriotTask);
+		launchingThread.start();
 	}
 }
