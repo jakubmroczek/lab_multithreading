01c565a1b071cbd31986726c0e7660bb489ec21a
Jakub Mroczek <jakub.mroczek@tomtom.com>
2019-06-05 22:59:51 +0200

    implemented BetterRadar

---
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -0,0 +1,24 @@
+package edu.iis.mto.multithread;
+
+import java.util.concurrent.Executor;
+import java.util.stream.IntStream;
+
+public class BetterRadar {
+    private PatriotBattery battery;
+    private Executor executor;
+
+    public BetterRadar(PatriotBattery battery, Executor executor)  {
+        this.battery = battery;
+        this.executor = executor;
+    }
+
+    public void notice(Scud enemyMissle) {
+        launchPatriot();
+    }
+
+    private void launchPatriot() {
+        executor.execute(() -> {
+            IntStream.range(0, 10).forEach(x -> battery.launchPatriot());
+        });
+    }
+}
